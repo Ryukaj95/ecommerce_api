8d7b8738adcbf803f5c1bc58803fe1324910e4d3
Francesco Cambi <francesco100@hotmail.it>
2017-05-22 16:58:09 +0200

    Flake8

--- models.py
+++ models.py
@@ -3,9 +3,6 @@ from peewee import DecimalField, TextField, CharField
 from peewee import UUIDField, ForeignKeyField, IntegerField
 from passlib.hash import pbkdf2_sha256
 
-from http.client import NO_CONTENT
-from http.client import CREATED
-
 database = SqliteDatabase('database.db')
 
 
